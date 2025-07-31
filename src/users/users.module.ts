9d0591c4f1a318b67956523e4990cbddb08c8969
John-Thailand <naoki.honda@mirrorfit.jp>
2025-07-31 21:41:47 +0900

    users.moduleでHashingProviderを使用できるようにするためにAuthModuleをimport

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -3,10 +3,11 @@ import { UsersController } from './users.controller';
 import { UsersService } from './users.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from './user.entity';
+import { AuthModule } from 'src/auth/auth.module';
 
 @Module({
   controllers: [UsersController],
   providers: [UsersService],
-  imports: [TypeOrmModule.forFeature([User])],
+  imports: [TypeOrmModule.forFeature([User]), AuthModule],
 })
 export class UsersModule {}
