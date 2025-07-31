7e48082abdd4254312c606977933414350c7b605
John-Thailand <naoki.honda@mirrorfit.jp>
2025-07-31 16:43:44 +0900

    ユーザーエンティティを作成

---
+++ src/users/user.entity.ts
@@ -0,0 +1,53 @@
+import {
+  Column,
+  CreateDateColumn,
+  DeleteDateColumn,
+  Entity,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+} from 'typeorm';
+
+@Entity('users')
+export class User {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @Column({
+    type: 'varchar',
+    length: 30,
+    nullable: false,
+  })
+  name: string;
+
+  // ユニーク制約で同じメールアドレスを登録できないようにする
+  @Column({
+    type: 'varchar',
+    length: 96,
+    unique: true,
+    nullable: false,
+  })
+  email: string;
+
+  @Column({
+    type: 'varchar',
+    length: 96,
+    nullable: false,
+  })
+  password: string;
+
+  @Column({
+    type: 'boolean',
+    nullable: false,
+    default: false,
+  })
+  is_administrator: boolean;
+
+  @CreateDateColumn()
+  created_at: Date;
+
+  @UpdateDateColumn()
+  updated_at: Date;
+
+  @DeleteDateColumn()
+  deleted_at: Date;
+}
