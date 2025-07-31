06c3963cf1eccab93b232bfbdd27fbb9cb0c2d54
John-Thailand <naoki.honda@mirrorfit.jp>
2025-07-31 16:54:56 +0900

    ユーザー作成DTOを実装

---
+++ src/users/dtos/create-user.dto.ts
@@ -0,0 +1,31 @@
+import {
+  IsEmail,
+  IsNotEmpty,
+  IsString,
+  Matches,
+  MaxLength,
+  MinLength,
+} from 'class-validator';
+
+export class CreateUserDto {
+  @IsString()
+  @IsNotEmpty()
+  @MinLength(3)
+  @MaxLength(30)
+  name: string;
+
+  @IsEmail()
+  @IsNotEmpty()
+  @MaxLength(96)
+  email: string;
+
+  @IsString()
+  @IsNotEmpty()
+  @MinLength(8)
+  @MaxLength(96)
+  @Matches(/^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!%*#?&]{8,}$/, {
+    message:
+      'Minimum eight characters, at least one letter, one number and one special character',
+  })
+  password: string;
+}
