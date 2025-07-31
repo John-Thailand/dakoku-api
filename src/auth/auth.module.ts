099f9c2138fadcf0183311a9b6af870545c6cad2
John-Thailand <naoki.honda@mirrorfit.jp>
2025-07-31 21:40:10 +0900

    HashingProviderをUsersServiceで使用できるようにexportsに設定

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,9 +1,20 @@
 import { Module } from '@nestjs/common';
 import { AuthController } from './auth.controller';
 import { AuthService } from './providers/auth.service';
+import { HashingProvider } from './providers/hashing.provider';
+import { BcryptProvider } from './providers/bcrypt.provider';
 
 @Module({
   controllers: [AuthController],
-  providers: [AuthService],
+  providers: [
+    AuthService,
+    // HashingProviderを依存注入できる
+    // 実際にインスタンス化されるのはBcryptProvider
+    {
+      provide: HashingProvider,
+      useClass: BcryptProvider,
+    },
+  ],
+  exports: [HashingProvider],
 })
 export class AuthModule {}
