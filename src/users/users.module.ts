3b14d1193877e4bdb649b654090a628a1a4f1ede
John-Thailand <naoki.honda@mirrorfit.jp>
2025-07-31 17:09:08 +0900

    TODOコメント追記

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -7,6 +7,7 @@ import { User } from './user.entity';
 @Module({
   controllers: [UsersController],
   providers: [UsersService],
+  // TODO: MYSQLとの繋ぎこみ
   imports: [TypeOrmModule.forFeature([User])],
 })
 export class UsersModule {}
