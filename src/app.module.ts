34dd033605133dd2a3806dbb1d8e1be7004ce9fd
John-Thailand <naoki.honda@mirrorfit.jp>
2025-08-02 12:43:20 +0900

    ConfigModuleでenvファイルを読み込み、カスタム設定ファイルを読み込む。ConfigService.getでカスタム設定ファイルのフィールドを読み込み、MYSQLとの接続設定を行う

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,24 +1,39 @@
 import { Module } from '@nestjs/common';
 import { UsersModule } from './users/users.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { User } from './users/user.entity';
 import { AuthModule } from './auth/auth.module';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import databaseConfig from './config/database.config';
 
 // 環境変数名（NODE_ENV）を取得
 const ENV = process.env.NODE_ENV;
 
 @Module({
   imports: [
+    ConfigModule.forRoot({
+      // ConfigModuleをアプリケーション全体でグローバルに利用可能にするため
+      // 各モジュールでConfigModuleを毎回importsしなくてよい
+      isGlobal: true,
+      envFilePath: !ENV ? '.env' : `.env.${ENV}`,
+      // カスタム設定ファイルを読み込む
+      load: [databaseConfig],
+    }),
     // TypeORMを使ってMySQLに接続するための初期設定
-    TypeOrmModule.forRoot({
-      type: 'mysql',
-      host: 'localhost',
-      port: 3306,
-      username: 'app_user',
-      password: 'app_password',
-      database: 'app_db',
-      entities: [User],
-      synchronize: true,
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        type: 'mysql',
+        host: configService.get('database.host'),
+        port: configService.get('database.port'),
+        username: configService.get('database.user'),
+        password: configService.get('database.password'),
+        database: configService.get('database.name'),
+        // entities: [User],
+        // @Entityを自動で読み込む設定 => entitiesにエンティティを羅列する必要がない
+        autoLoadEntities: configService.get('database.autoLoadEntities'),
+        synchronize: configService.get('database.synchronize'),
+      }),
     }),
     UsersModule,
     AuthModule,
