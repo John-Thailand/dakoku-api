bd3f3735babf03e1b9e18d0269104318a4bdf428
John-Thailand <naoki.honda@mirrorfit.jp>
2025-08-04 18:17:33 +0900

    mailConfigを設定

--- src/config/environment.validation.ts
+++ src/config/environment.validation.ts
@@ -11,4 +11,7 @@ export default Joi.object({
   DB_NAME: Joi.string().required(),
   DB_SYNC: Joi.boolean().required(),
   DB_AUTOLOAD_ENTITIES: Joi.boolean().required(),
+  MAIL_HOST: Joi.string().required(),
+  SMTP_USERNAME: Joi.string().required(),
+  SMTP_PASSWORD: Joi.string().required(),
 });
