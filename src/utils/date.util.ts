4255e424e4ee4e41883649eceddfc99bba0fa7b6
John-Thailand <naoki.honda@mirrorfit.jp>
2025-08-10 01:24:46 +0900

    DateUtilに新しいメソッドを追加

--- src/utils/date.util.ts
+++ src/utils/date.util.ts
@@ -4,12 +4,20 @@ import { toZonedTime } from 'date-fns-tz';
 const DEFAULT_TZ = 'Asia/Tokyo';
 
 export class DateUtil {
-  static getTargetMonth(
+  static getTargetMonthText(
     baseDate: Date = new Date(),
     timeZone: string = DEFAULT_TZ,
   ): string {
+    const monthStart = this.getTargetMonthDate(baseDate, timeZone);
+    return format(monthStart, 'yyyy-MM-01');
+  }
+
+  static getTargetMonthDate(
+    baseDate: Date = new Date(),
+    timeZone: string = DEFAULT_TZ,
+  ): Date {
     const zonedTime = toZonedTime(baseDate, timeZone);
     const monthStart = startOfMonth(zonedTime);
-    return format(monthStart, 'yyyy-MM-01');
+    return monthStart;
   }
 }
