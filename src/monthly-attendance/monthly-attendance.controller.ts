926fe788506a1ce29c9970773728b674823efae8
John-Thailand <naoki.honda@mirrorfit.jp>
2025-08-10 10:58:52 +0900

    自身の月次勤怠を締めるAPIのエンドポイントを実装

--- src/monthly-attendance/monthly-attendance.controller.ts
+++ src/monthly-attendance/monthly-attendance.controller.ts
@@ -1,4 +1,18 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Put, Param, Req, UseGuards } from '@nestjs/common';
+import { REQUEST_USER_KEY } from 'src/auth/constants/constants';
+import { AuthGuard } from 'src/auth/guards/auth.guard';
 
-@Controller('monthly-attendance')
-export class MonthlyAttendanceController {}
+@Controller()
+export class MonthlyAttendanceController {
+  @Put('users/me/monthly-attendance/:targetMonth')
+  @UseGuards(AuthGuard)
+  public closeMyMonthlyAttendance(
+    @Req() request,
+    // TODO: バリデーション
+    @Param('targetMonth') targetMonth: string,
+  ) {
+    const requestUser = request[REQUEST_USER_KEY];
+    console.log(requestUser.sub);
+    console.log(targetMonth);
+  }
+}
--- src/monthly-attendance/monthly-attendance.module.ts
+++ src/monthly-attendance/monthly-attendance.module.ts
@@ -4,9 +4,21 @@ import { MonthlyAttendanceService } from './monthly-attendance.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { MonthlyAttendance } from './monthly-attendance.entity';
 import { User } from 'src/users/user.entity';
+import { AuthModule } from 'src/auth/auth.module';
+import { ConfigModule } from '@nestjs/config';
+import jwtConfig from 'src/auth/config/jwt.config';
+import { JwtModule } from '@nestjs/jwt';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([MonthlyAttendance, User])],
+  imports: [
+    TypeOrmModule.forFeature([MonthlyAttendance, User]),
+    AuthModule,
+    // jwtConfigをAuthModuleで使えるようにする
+    ConfigModule.forFeature(jwtConfig),
+    // ConfigServiceからjwtConfigを読み取る
+    // そのjwtConfigをJwtModuleに渡す
+    JwtModule.registerAsync(jwtConfig.asProvider()),
+  ],
   controllers: [MonthlyAttendanceController],
   providers: [MonthlyAttendanceService],
   exports: [MonthlyAttendanceService],
