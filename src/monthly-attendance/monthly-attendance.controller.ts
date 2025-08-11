7f3abc53bb7f1a59b6d7220762ba907d8434e672
John-Thailand <naoki.honda@mirrorfit.jp>
2025-08-11 23:24:53 +0900

    コメント追加

--- src/monthly-attendance/monthly-attendance.controller.ts
+++ src/monthly-attendance/monthly-attendance.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Put, Param, Req, UseGuards } from '@nestjs/common';
+import { Controller, Param, Req, UseGuards, Patch } from '@nestjs/common';
 import { REQUEST_USER_KEY } from 'src/auth/constants/constants';
 import { AuthGuard } from 'src/auth/guards/auth.guard';
 import { CloseMyMonthlyAttendanceDto } from 'src/users/dtos/close-my-monthly-attendance.dto';
@@ -11,7 +11,9 @@ export class MonthlyAttendanceController {
     private readonly monthlyAttendanceService: MonthlyAttendanceService,
   ) {}
 
-  @Put('users/me/monthly-attendance/:target_month')
+  // statusのみの部分更新なのでPatchを設定する
+  // 月次勤怠のstatusを更新するということで/statusも追加しておく
+  @Patch('users/me/monthly-attendance/:target_month/status')
   @UseGuards(AuthGuard)
   public closeMyMonthlyAttendance(
     @Req() request,
