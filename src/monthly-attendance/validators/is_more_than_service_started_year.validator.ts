83d336c37d704051a21cfeb4d400020d3471cf6b
John-Thailand <naoki.honda@mirrorfit.jp>
2025-08-11 13:01:10 +0900

    文字列であるかのチェックも追加

--- src/monthly-attendance/validators/is_more_than_service_started_year.validator.ts
+++ src/monthly-attendance/validators/is_more_than_service_started_year.validator.ts
@@ -10,6 +10,10 @@ export function IsMoreThanServiceStartedYear() {
       constraints: [], // 外部から渡された制約値
       validator: {
         validate(value: any, _args: ValidationArguments) {
+          // 文字列であるかチェック
+          if (typeof value !== 'string') {
+            return false;
+          }
           // 勤怠サービスが開始された年以上であるか確認
           const splitedValue = value.split(HYPHEN);
           const year = Number(splitedValue[0]);
