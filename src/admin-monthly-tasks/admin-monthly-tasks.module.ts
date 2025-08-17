e0d9e38e6b78fba8474039ecbcac9054306414ac
John-Thailand <naoki.honda@mirrorfit.jp>
2025-08-17 18:52:27 +0900

    Moduleでimportsとexportsの設定追加

--- src/admin-monthly-tasks/admin-monthly-tasks.module.ts
+++ src/admin-monthly-tasks/admin-monthly-tasks.module.ts
@@ -1,7 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AdminMonthlyTasksService } from './admin-monthly-tasks.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { AdminMonthlyTask } from './admin-monthly-task.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([AdminMonthlyTask])],
   providers: [AdminMonthlyTasksService],
+  exports: [AdminMonthlyTasksService],
 })
 export class AdminMonthlyTasksModule {}
