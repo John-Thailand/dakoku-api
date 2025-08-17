0ecb70b0482f7c2ef1fb6bb2ad285e0417edd663
John-Thailand <naoki.honda@mirrorfit.jp>
2025-08-17 18:54:35 +0900

    BatchProcessingModuleでAdminMonthlyTasksModuleをimport

--- src/batch-processing/batch-processing.module.ts
+++ src/batch-processing/batch-processing.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { BatchProcessingController } from './batch-processing.controller';
 import { BatchProcessingService } from './providers/batch-processing.service';
 import { MonthlyAttendanceModule } from 'src/monthly-attendance/monthly-attendance.module';
+import { AdminMonthlyTasksModule } from 'src/admin-monthly-tasks/admin-monthly-tasks.module';
 
 @Module({
-  imports: [MonthlyAttendanceModule],
+  imports: [MonthlyAttendanceModule, AdminMonthlyTasksModule],
   controllers: [BatchProcessingController],
   providers: [BatchProcessingService],
 })
