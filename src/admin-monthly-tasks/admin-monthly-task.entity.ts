41063a6797b17ef148d43bdf70e5444fb8c9422e
John-Thailand <naoki.honda@mirrorfit.jp>
2025-08-17 14:04:52 +0900

    AdminMonthlyTaskエンティティを実装

---
+++ src/admin-monthly-tasks/admin-monthly-task.entity.ts
@@ -0,0 +1,38 @@
+import {
+  Column,
+  CreateDateColumn,
+  DeleteDateColumn,
+  Entity,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+} from 'typeorm';
+import { AdminMonthlyTaskStatus } from './enums/admin-monthly-task-status.enum';
+
+@Entity('admin_monthly_tasks')
+export class AdminMonthlyTask {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @Column({
+    type: 'date',
+    nullable: false,
+  })
+  target_month: Date;
+
+  @Column({
+    type: 'enum',
+    enum: AdminMonthlyTaskStatus,
+    nullable: false,
+    default: AdminMonthlyTaskStatus.PENDING,
+  })
+  status: AdminMonthlyTaskStatus;
+
+  @CreateDateColumn()
+  created_at: Date;
+
+  @UpdateDateColumn()
+  updated_at: Date;
+
+  @DeleteDateColumn()
+  deleted_at: Date;
+}
---
+++ src/admin-monthly-tasks/enums/admin-monthly-task-status.enum.ts
@@ -0,0 +1,5 @@
+export enum AdminMonthlyTaskStatus {
+  PENDING = 'pending',
+  CLOSE_ATTENDANCE = 'close_attendance',
+  SUBMIT_ATTENDANCE_SHEET = 'submit_attendance_sheet',
+}
